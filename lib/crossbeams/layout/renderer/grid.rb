2adf0869541c36e17a4bb1f189d2975ae0bd4b32
James Silberbauer <jamessil@telkomsa.net>
2018-01-21 18:16:45 +0200

    Reduce buttons on grid header to icon width.
    Scroll-to-column uses button and hidden list instead of select element.

--- lib/crossbeams/layout/renderer/grid.rb
+++ lib/crossbeams/layout/renderer/grid.rb
@@ -31,8 +31,8 @@ module Crossbeams
           if options[:print_button]
             raise ArgumentError, 'print_url is required to print a grid' unless options[:print_url]
             print_section = <<~HTML
-              <label style="margin-left: 20px;">
-                  <button class="pure-button" onclick="crossbeamsGridEvents.printAGrid('#{grid_id}', '#{options[:print_url]}')"><i class="fa fa-print"></i> Print</button>
+              <label style="margin-left: 10px;">
+                  <button class="pure-button" onclick="crossbeamsGridEvents.printAGrid('#{grid_id}', '#{options[:print_url]}')" title="Print"><i class="fa fa-print"></i></button>
               </label>
             HTML
           else
@@ -41,26 +41,26 @@ module Crossbeams
 
           <<-HTML
           <div class="grid-head">
-            <label style="margin-left: 20px;">
+            <label style="margin-left: 10px;">
                 <button class="crossbeams-to-fullscreen" onclick="crossbeamsGridEvents.toFullScreen('#{grid_id}')" title="show in fullscreen mode"><i class="fa fa-arrows-alt"></i></button>
             </label>
-            <label style="margin-left: 20px;">
+            <label style="margin-left: 10px;">
                 <button class="crossbeams-view-row" onclick="crossbeamsGridEvents.viewSelectedRow('#{grid_id}')" title="view selected row"><i class="fa fa-eye"></i></button>
             </label>#{save_multiselect_button(grid_id, options)}
-            <label style="margin-left: 20px;">
-                <button class="pure-button" onclick="crossbeamsGridEvents.csvExport('#{grid_id}', '#{Grid.file_name_from_caption(caption)}')"><i class="fa fa-file"></i> Export to CSV</button>
+            <label style="margin-left: 10px;">
+                <button class="pure-button" onclick="crossbeamsGridEvents.csvExport('#{grid_id}', '#{Grid.file_name_from_caption(caption)}')" title="Export to CSV"><i class="fa fa-file"></i></button>
             </label>
-            <label style="margin-left: 20px;">
-                <button class="pure-button" onclick="crossbeamsGridEvents.toggleToolPanel('#{grid_id}')"><i class="fa fa-cog"></i> Tool panel</button>
+            <label style="margin-left: 10px;">
+                <button class="pure-button" onclick="crossbeamsGridEvents.toggleToolPanel('#{grid_id}')" title="Tool panel"><i class="fa fa-cog"></i></button>
             </label>
             #{print_section}
-            <label style="margin-left: 20px;">
-                <input class="un-formed-input" onkeyup="crossbeamsGridEvents.quickSearch(event)" placeholder='Search...' data-grid-search="true" data-grid-id="#{grid_id}"/>
+            <label class="crossbeams-column-jump" style="margin-left: 10px;">
+                <button><i class="fa fa-arrows-h" title="Scroll to column"></i>
+                </button>
+                <ul id='#{grid_id}-scrollcol' data-grid-id="#{grid_id}" class="crossbeams-column-jump-list"></ul>
             </label>
-            <label style="margin-left: 20px;">
-                <select id='#{grid_id}-scrollcol' onchange="crossbeamsGridEvents.scrollToColumn(event)" data-grid-id="#{grid_id}"/>
-                  <option value=''>Scroll to column</option>
-                <select>
+            <label style="margin-left: 10px;">
+                <input class="un-formed-input" onkeyup="crossbeamsGridEvents.quickSearch(event)" placeholder='Search...' data-grid-search="true" data-grid-id="#{grid_id}"/>
             </label>
             <span class="grid-caption">
               #{caption}
@@ -72,7 +72,7 @@ module Crossbeams
 
         def render
           head_section = Grid.header(@grid_id, @caption,
-                                     print_button: true,
+                                     print_button: false, # This turned off for now
                                      print_url: @url,
                                      multiselect: @multiselect,
                                      multiselect_url: @multiselect_url,
@@ -98,7 +98,7 @@ module Crossbeams
         def self.save_multiselect_button(grid_id, options)
           return '' unless options[:multiselect]
           <<~HTML
-            <label style="margin-left: 20px;">
+            <label style="margin-left: 10px;">
                 <button class="crossbeams-view-savemulti" onclick="crossbeamsGridEvents.saveSelectedRows('#{grid_id}', '#{options[:multiselect_url]}', #{options[:can_be_cleared] == true}, #{options[:multiselect_save_remote] == true})" title="save selection"><i class="fa fa-save"></i></button>
             </label>
           HTML
