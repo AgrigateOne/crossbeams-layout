b91aea0f05559a66ca9b83126b42ed711cfd2aef
James Silberbauer <jamessil@telkomsa.net>
2020-08-05 10:41:40 +0200

    Fix CallbackSection DOM id

--- lib/crossbeams/layout/callback_section.rb
+++ lib/crossbeams/layout/callback_section.rb
@@ -31,7 +31,7 @@ module Crossbeams
         <<-HTML
           <section id="section-#{sequence}" class="crossbeams_layout">
           <h2>#{caption}</h2>
-          #{LoadingMessage.new(id: "crossbeams_callback_target_#{sequence}").render}
+          #{LoadingMessage.new(dom_id: "crossbeams_callback_target_#{sequence}").render}
           </section>
           <script>
             document.addEventListener('DOMContentLoaded', () => {
