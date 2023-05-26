e62c7ba43ed38731a3af956c5a738b32af5d4e2e
James Silberbauer <jamessil@telkomsa.net>
2023-05-26 12:19:07 +0200

    Missing file

---
+++ lib/crossbeams/layout/renderer/select_multiple.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+module Crossbeams
+  module Layout
+    module Renderer
+      # Render a select Field.
+      class SelectMultiple < Base # rubocop:disable Metrics/ClassLength
+        def configure(field_name, field_config, page_config)
+          @field_name = field_name
+          @field_config = field_config
+          @page_config = page_config
+          @caption = field_config[:caption] || present_field_as_label(field_name)
+        end
+
+        def render
+          attrs = apply_attrs # For class, prompt etc...
+          sel = Array(@field_config[:selected] || form_object_value)
+          sel = Array(override_with_form_value(sel))
+
+          # disabled_option = find_disabled_option(sel, @field_config[:disabled_options])
+
+          # render_string(attrs, sel, disabled_option)
+          render_string(attrs, sel, nil)
+        end
+
+        private
+
+        def apply_attrs
+          attrs = [] # For class, prompt etc...
+          cls   = apply_classes
+          attrs << "class=\"#{cls.join(' ')}\"" unless cls.empty?
+          attrs << disabled
+          attrs << required
+          attrs << behaviours
+          attrs << sort_items
+          attrs.compact
+        end
+
+        def disabled
+          return nil unless @field_config[:disabled] && @field_config[:disabled] == true
+
+          'disabled="true"'
+        end
+
+        def required
+          return nil unless @field_config[:required] && @field_config[:required] == true
+
+          'required="true"'
+        end
+
+        def apply_classes
+          cls = []
+          cls << 'searchable-multi-select'
+          cls
+        end
+
+        def render_string(attrs, sel, disabled_option)
+          <<-HTML
+          <div #{wrapper_id} class="#{div_class}"#{css_style}#{wrapper_visibility}>#{hint_text}
+            #{backup_empty_select}
+            <select #{attrs.join(' ')} #{name_attribute_multi} #{field_id} multiple>
+            #{make_prompt}#{build_options(@field_config[:options], sel, disabled_option)}
+            </select>
+            <label for="#{id_base}">#{@caption}#{error_state}#{hint_trigger}</label>
+          </div>
+          HTML
+        end
+
+        def backup_empty_select
+          # Hidden blank value to be submitted as a param if the Searchable component box is cleared.
+          %(<input #{name_attribute_multi} type="hidden" value="">)
+        end
+
+        def make_prompt
+          return '' unless @field_config[:prompt]
+
+          str = @field_config[:prompt].is_a?(String) ? @field_config[:prompt] : 'Select one or more values'
+          "<option value=\"\" placeholder>#{str}</option>\n"
+        end
+
+        def sort_items
+          sort_items = @field_config.fetch(:sort_items, true) ? 'Y' : 'N'
+          %( data-sort-items="#{sort_items}")
+        end
+
+        def css_style
+          return '' unless @field_config[:min_charwidth]
+
+          %( style="min-width:#{@field_config[:min_charwidth]}rem;")
+        end
+
+        def build_options(list, selected = [], disabled_option = nil)
+          if list.is_a?(Hash)
+            grp_disabled = disabled_option
+            opts = []
+            list.each do |group, sublist|
+              opts << %(<optgroup label="#{group}">)
+              opts << make_options(Array(sublist), selected, grp_disabled)
+              opts << '</optgroup>'
+              grp_disabled = nil # This might not be quite the right position...
+            end
+            opts.join("\n")
+          else
+            make_options(Array(list), selected, disabled_option)
+          end
+        end
+
+        def make_options(list, selected, disabled_option)
+          # disabled = disabled_string(disabled_option, selected)
+          opts = list.map do |a|
+            a.is_a?(Array) ? option_string(a.first, a.last, selected) : option_string(a, a, selected)
+          end
+          # opts.unshift(disabled) unless disabled.nil?
+          opts.join("\n")
+        end
+
+        def option_string(text, value, selected, disabled = '')
+          sel = selected.map(&:to_s).include?(value.to_s) ? ' selected ' : ''
+          "<option value=\"#{CGI.escapeHTML(value.to_s)}\"#{sel}#{disabled}>#{CGI.escapeHTML(text.to_s)}</option>"
+        end
+
+        def find_disabled_options(sel, disabled_list)
+          return nil if disabled_list.nil? || disabled_list.empty?
+          return nil if sel.nil? || sel.empty?
+
+          if disabled_list.first.is_a? Array
+            disabled_list.rassoc(sel)
+          elsif disabled_list.include?(sel)
+            sel
+          end
+        end
+
+        def disabled_string(disabled_option, selected)
+          return nil if disabled_option.nil?
+
+          if disabled_option.is_a?(Array)
+            option_string(disabled_option.first, disabled_option.last, selected, ' disabled')
+          else
+            option_string(disabled_option, disabled_option, selected, ' disabled')
+          end
+        end
+      end
+    end
+  end
+end
