37bafffad9f39061446e764232a9d827fd9f9aa1
James Silberbauer <jamessil@telkomsa.net>
2026-06-30 17:18:43 +0200

    Filter: make the relationships between DOM elements clearer

--- lib/crossbeams/layout/filter_field.rb
+++ lib/crossbeams/layout/filter_field.rb
@@ -23,7 +23,7 @@ module Crossbeams
         prepare_options
         prepare_disabled
         super()
-        # @caption = options.fetch(:caption, field_name.to_s.capitalize.gsub('_', ' '))
+        @has_selection = !(options[:selected] || []).empty?
         @nodes = []
         # @items = options[:items]
         # @selected = options[:selected] || []
@@ -37,17 +37,21 @@ module Crossbeams
         false
       end
 
+      def filter_applied?
+        @has_selection
+      end
+
       def render
         # swap inline-flex  for hidden to show...
         attrs = [%(class="w-full searchable-multi-select")]
         #   #{backup_empty_select}
         <<~HTML
           <div class="relative">
-            <div id="filter_field_#{field_name}" data-filter-wrapper="Y" class="border border-slate-600 rounded-full p-2 flex items-center">
-              <label class="" data-filter-field="Y">#{caption}</label>
-              <div class="inline-flex" data-filter-label-val="Y">#{selected_values(prefix: true)}</div>
+            <div id="filter_field_#{field_name}" data-filter-wrapper="Y" data-filter-set="#{field_name}" class="border border-slate-600 rounded-full p-2 flex items-center">
+              <label class="" data-filter-field="Y" data-filter-set="#{field_name}">#{caption}</label>
+              <div class="inline-flex" data-filter-label-val="Y" data-filter-set="#{field_name}">#{selected_values(prefix: true)}</div>
             </div>
-            <div class="hidden absolute min-w-96" data-filter-lookup="Y">
+            <div class="hidden absolute min-w-96" data-filter-lookup="Y" data-filter-lkp-set="#{field_name}">
               <select #{attrs.join(' ')} #{name_attribute_multi} #{field_id} data-filter-select="Y" multiple>
               #{build_1_or_2_options}
               </select>
@@ -94,7 +98,7 @@ module Crossbeams
         return '' if items.empty?
 
         if prefix
-          %(<span class="px-1">| #{items.map { |i| i.is_a?(Array) ? i.first : i }.join(', ')}</span>)
+          %(<span class="px-1" data-filter-set="#{field_name}">| #{items.map { |i| i.is_a?(Array) ? i.first : i }.join(', ')}</span>)
         else
           items.map { |i| i.is_a?(Array) ? i.first : i }.join(', ')
         end
